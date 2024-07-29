d01c8c91998ea177c46c781229c466b2d334a544
Mhideyy <114811594+Mhideyy@users.noreply.github.com>
2024-07-29 20:24:37 +0100

    Add files via upload

---
+++ axia africa test part 2/index.js
@@ -0,0 +1,29 @@
+myStudents = [  "samuel",
+                "ayomide",
+                "collins",
+                "david",
+                "amarachi",
+                "bola",
+                "daniel",
+                "james",
+                "liam",
+                "kemi"
+]
+for (let i = 0; i < 10; i++) {
+                                console.log(myStudents[i]);
+}
+
+
+// object
+
+let details = {
+    name: "John",
+    age: 25,
+    married: false,
+    address: {}
+}
+
+console.log(details.name);
+console.log(details.age);
+console.log(details.married);
+console.log(details.address);
